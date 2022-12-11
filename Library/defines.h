d6c6f911fee98527092e0a2919967f752de80f24
Amr Abdelhakim <amr_abdelhakim4@yahoo.com>
2022-12-11 22:20:36 +0200

    Adding definitions to defines.h and updating STD_TYPES.h

---
+++ Library/defines.h
@@ -0,0 +1,15 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+
+#define ENABLED 		1u
+#define DISABLED		0u
+#define NULL	((void*)0)
+
+#define OK		0u
+#define NOK		1u
+#define NULL_PTR_ERR	2u
+
+
+
+#endif
